1829430ff0907c13b03e60b48d6287bedfa870f8
Yqloss <yqloss@yqloss.net>
2025-07-16 21:03:13 +0800

    fix(Scripts): fix installation deleting Settings.xml file

--- Scripts/install-disguise.ps1
+++ Scripts/install-disguise.ps1
@@ -5,6 +5,6 @@ $root = "$PSScriptRoot/.."
 $game = (Get-Content -Path "$root/GameFolder.txt").Trim()
 $id = (Get-Content -Path "$root/Disguise.json" | ConvertFrom-Json).Id
 
-Remove-Item -Path "$game/Mods/$id" -Recurse -Force
+Remove-Item -Path "$game/Mods/$id" -Recurse -Force -Exclude "Settings.xml"
 New-Item -Path "$game/Mods/$id" -ItemType Directory
 Copy-Item -Path "$root/build/files/*" -Destination "$game/Mods/$id" -Force
